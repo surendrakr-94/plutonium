ea22fabe421c5495a5b1c98563f1a97c8bbb395a
Surendra Kumar <surendrakr-94>
2022-08-14 17:23:09 +0530

    new change

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const UserModel= require("../models/userModel.js")
+const BookModel= require("../models/bookModel")
 const UserController= require("../controllers/userController")
+const BookController = require("../controllers/bookController");
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -10,5 +13,8 @@ router.get("/test-me", function (req, res) {
 router.post("/createUser", UserController.createUser  )
 
 router.get("/getUsersData", UserController.getUsersData)
+router.post("/createNewBook", BookController.createNewBook);
+
+router.get("/getAllBooks", BookController.getNewBook);
 
 module.exports = router;
